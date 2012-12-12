e791e625797b2e0d0278dbe4681a388800980c3b
Thomas Bollmeier <tbollmeier@web.de>
2012-12-12 22:10:42 +0100

    output pt.2

--- src/gobjcreator3/codegen/c_code_generator.py
+++ src/gobjcreator3/codegen/c_code_generator.py
@@ -1,5 +1,7 @@
 from gobjcreator3.codegen.code_generator import CodeGenerator
+from gobjcreator3.model.visibility import Visibility
 import os
+import fabscript
 
 class CCodeGenerator(CodeGenerator):
     
@@ -11,7 +13,8 @@ class CCodeGenerator(CodeGenerator):
         self._cur_dir = ""
         
         self._name_creator = NameCreator()
-        
+        self._refresh_template_processor()
+                
     def generate(self):
         
         self._generate_module(self._root_module)
@@ -28,10 +31,14 @@ class CCodeGenerator(CodeGenerator):
         
         for m in module.modules:
             self._generate_module(m)
+            
+        self._refresh_template_processor()
+        self._setup_module_symbols(module)
         
         objs = [obj for obj in module.objects if obj.filepath_origin == self._origin]
         
         for obj in objs:
+            self._setup_gobject_symbols(obj)
             self._gen_object_header(obj)
             self._gen_object_source(obj)
                     
@@ -46,8 +53,8 @@ class CCodeGenerator(CodeGenerator):
     def _gen_object_header(self, obj):
         
         file_path = self._cur_dir + os.sep + self._name_creator.create_obj_header_name(obj)
-        lines = []
-        
+        lines = self._get_lines_from_template("gobject_header.template")
+                
         self._out.visit_text_file(file_path, lines)
             
     def _gen_object_source(self, obj):
@@ -57,34 +64,57 @@ class CCodeGenerator(CodeGenerator):
         
         self._out.visit_text_file(file_path, lines)
         
+    def _get_lines_from_template(self, template_file):
+        
+        template_path = os.path.dirname(__file__) + os.sep + "templates" + os.sep + "c"
+        template_path += os.sep + template_file
+        template_path = os.path.abspath(template_path)
+        
+        out_buffer = self._template_processor.createStringOut()
+        self._template_processor.createCode(template_path, out_buffer)
+        
+        return out_buffer.content.split(os.linesep)
+        
+    def _refresh_template_processor(self):
+        
+        self._template_processor = fabscript.API()
+        self._template_processor.setEditableSectionStyle(self._template_processor.Language.C)
+
+        self._template_processor["PUBLIC"] = Visibility.PUBLIC
+        self._template_processor["PROTECTED"] = Visibility.PROTECTED
+        self._template_processor["PRIVATE"] = Visibility.PRIVATE
+        
+    def _setup_module_symbols(self, module):
+        
+        camel_case_prefix = module.name.capitalize()
+        curmod = module
+        while curmod.module:
+            curmod = curmod.module
+            if curmod.name:
+                camel_case_prefix = curmod.name.capitalize() + camel_case_prefix
+                
+        prefix = self._name_creator.replace_camel_case(camel_case_prefix, "_")
+                    
+        self._template_processor["module_prefix"] = prefix.lower()
+        self._template_processor["MODULE_PREFIX"] = prefix.upper() 
+        self._template_processor["ModulePrefix"] = camel_case_prefix
+        self._template_processor["has_module"] = bool(module.name)
+        
+    def _setup_gobject_symbols(self, obj):
+        
+        self._template_processor["class"] = obj
+        self._template_processor["ClassName"] = obj.name
+        self._template_processor["CLASS_NAME"] = self._name_creator.replace_camel_case(obj.name, "_").upper()
+        prefix = obj.cfunc_prefix or self._name_creator.replace_camel_case(obj.name, "_").lower()
+        self._template_processor["class_prefix"] = prefix
+        
 class NameCreator(object):
     
     def __init__(self):
         
         self._file_name_sep = "-"
-    
-    def create_obj_header_name(self, obj):
-        
-        return self._create_elem_base_name(obj) + ".h"
 
-    def create_obj_source_name(self, obj):
-        
-        return self._create_elem_base_name(obj) + ".c"
-        
-    def _create_elem_base_name(self, module_elem):
-        
-        res = self._replace_camel_case(module_elem.name, self._file_name_sep)
-        
-        module = module_elem.module
-        while module and module.name:
-            res = module.name + self._file_name_sep + res
-            module = module.module
-            
-        res = res.lower()
-        
-        return res 
-    
-    def _replace_camel_case(self, text, replace_char="_"):
+    def replace_camel_case(self, text, replace_char="_"):
         
         res = ""
         
@@ -96,3 +126,24 @@ class NameCreator(object):
             prev = ch
             
         return res
+    
+    def create_obj_header_name(self, obj):
+        
+        return self._create_elem_base_name(obj) + ".h"
+
+    def create_obj_source_name(self, obj):
+        
+        return self._create_elem_base_name(obj) + ".c"
+        
+    def _create_elem_base_name(self, module_elem):
+        
+        res = self.replace_camel_case(module_elem.name, self._file_name_sep)
+        
+        module = module_elem.module
+        while module and module.name:
+            res = module.name + self._file_name_sep + res
+            module = module.module
+            
+        res = res.lower()
+        
+        return res 
